82fc14f2402527d6489e7b263a8f80ef59ac888e
DanielCatz <daniel.caterson@gmail.com>
2017-08-11 10:57:22 -0700

    test

--- app.py
+++ app.py
@@ -20,8 +20,68 @@ q = Queue(connection = conn)
 
 from models import *
 
+def count_and_save_words(url):
+
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing    
+            raw = BeautifulSoup(r.text)
+            for script in raw(["script","sytle"]):
+            	script.extract()
+            myText = raw.get_text()
+            outputs.append(myText)
+            nltk.data.path.append('./nltk_data/')  # set the path
+            tokens = nltk.word_tokenize(myText)
+            text = nltk.Text(tokens)
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+
+    return render_template('index.html', results=results)
+
+@app.route('/bob', methods=['GET', 'POST'])
+def bob():
     errors = []
     outputs =[]
     results = {}
@@ -57,7 +117,7 @@ def index():
                 no_stop_words_count.items(),
                 key=operator.itemgetter(1),
                 reverse=True
-            )
+            )[:10]
             try:
                 result = Result(
                     url=url,
@@ -69,8 +129,6 @@ def index():
             except:
                 errors.append("Unable to add item to database.")
     return render_template('index.html', errors=errors, results=results)
-
-
 
 
 @app.route("/results/<job_key>", methods=['GET'])
